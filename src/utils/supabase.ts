c708442ce54f25b249cab053869fd2db1f90dbb7
JUNYI OU <49964599+Irvingouj@users.noreply.github.com>
2023-06-01 19:38:10 -0400

    fix: remove middleware for now

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,9 +1,11 @@
-import { Database } from '@/lib/database.types'
-import { createClient } from '@supabase/supabase-js'
+import { Database } from '@/lib/database.types';
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 if (!supabaseKey || !supabaseUrl) {
-  throw new Error('Missing SUPABASE_KEY or URL')
+  throw new Error(`
+  Missing env vars. Check NEXT_PUBLIC_SUPABASE_URL ${supabaseUrl} and
+  NEXT_PUBLIC_SUPABASE_ANON_KEY ${supabaseKey} are defined`);
 }
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
